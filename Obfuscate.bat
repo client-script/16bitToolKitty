2af94bce54b6e598a5f490cc097e53ad7f831c3c
client-script <theomegatrull@gmail.com>
2025-09-22 00:00:12 +1000

    Create Obfuscate.bat

---
+++ Obfuscate.bat
@@ -0,0 +1,14 @@
+:: basic obfuscation script
+REM grace.likes.panda discord
+@echo off
+if "%~1"=="" exit /b
+if /i "%~x1" neq ".bat" if /i "%~x1" neq ".cmd" exit /b
+for /f %%i in ("certutil.exe") do if not exist "%%~$path:i" (
+  echo CertUtil.exe not found.
+  pause
+  exit /b
+)
+>"temp.~b64" echo(//4mY2xzDQo=
+certutil.exe -f -decode "temp.~b64" "%~n1-Obfuscated%~x1"
+del "temp.~b64"
+copy "%~n1-Obfuscated%~x1" /b + "%~1" /b
